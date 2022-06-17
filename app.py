46f6fb3bba2c4ed3701388e00b606f4944adb9dc
Arslan Arshad <arslanarshad07@gmail.com>
2022-06-18 03:14:28 +0500

    First one

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename='filename.log', level=logging.DEBUG)
 def hello_world():
     logging.info("Data received is mentioned bellow")
     logging.debug(request.data)
-    return "Hi there, My name is Arslan Arshad"
+    return "OK"
 
 
 app.run(debug=False, host='0.0.0.0', port=5000)
